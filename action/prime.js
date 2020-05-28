c1aa981c1c39a5d3e9f26c238ee1cf9972d2fe4d
Hima-Bindu97 <himabindureddygotike@msitprogram.net>
2020-05-28 15:16:43 +0530

    done

---
+++ action/prime.js
@@ -0,0 +1,28 @@
+function isPrime(n)
+{
+
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }
+  else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+  var assert=require('assert');
+  assert(isPrime(5),"Testcase1 passed");
+  assert(isPrime(12),"Testcase2 passed");
+  assert(isPrime(0),"Testcase3 passed");
+  assert(isPrime(20),"Testcase4 passed");
