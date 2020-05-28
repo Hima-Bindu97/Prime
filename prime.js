6ed35175061e2eb4555f6fa42c6e157e0a3e6fcb
Hima-Bindu97 <himabindureddygotike@msitprogram.net>
2020-05-28 21:36:06 +0530

    changes made

--- prime.js
+++ prime.js
@@ -1,31 +1,28 @@
-function isPrime(n)
-{
-
-  if (n===1)
-  {
-    return false;
-  }
-  else if(n === 2)
-  {
-    return true;
-  }
-  else
-  {
-    for(var x = 2; x < n; x++)
-    {
-      if(n % x === 0)
-      {
-        return false;
+function isPrime(num){
+    if (num===2) {
+      return true;
+    }
+    else if(num>1){
+      for(var i=2;i<num;i++){
+        if(num%i!==0 ) {
+          return true;
+        }
+        else if(num===i*i){
+          return false
+        }
+        else {
+          return false;
+        }
       }
     }
-    return true;  
+    else{
+      return false;
+    }
   }
-}
-  var assert=require('assert');
-  assert(isPrime(5),"Testcase1 passed");
-  assert(isPrime(12),"Testcase2 passed");
-  assert(isPrime(0),"Testcase3 passed");
-  assert(isPrime(20),"Testcase4 passed");
 
-
-  console.log("All testcases passed");
+var assert=require('assert') 
+assert(isPrime(5),"Testcase1 passed");
+assert(isPrime(12)===false,"Testcase2 passed");
+assert(isPrime(0)===false,"Testcase3 passed");
+assert(isPrime(20)===false,"Testcase4 passed");
+console.log("All testcases passed");
